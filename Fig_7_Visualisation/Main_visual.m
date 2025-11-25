1577306a9d9b953228f565b68835ed5e8b912935
ether3333 <ether@kaist.ac.kr>
2025-11-25 22:06:54 +0900

    dd

--- Fig_7_Visualisation/Main_visual.m
+++ Fig_7_Visualisation/Main_visual.m
@@ -111,75 +111,75 @@ end
 
 
 %% Output Result (2): Animation from time 0 to final converged time
-close all
-figure('position', [650, 10, 400, 350]);hold on;
-grid on;daspect([1 1 1]);axis square
-axis([-t_location_limit(1) t_location_limit(1) -t_location_limit(2) t_location_limit(2)]*1.1)
-axis square
-grid on;
-UAV_ID = cellstr(num2str([1:n]'));
-[UAV_1,UAV_2] = find(triu(MST)>0);
-n_Edge = length(UAV_1);
-dx = 4; dy = 1;
-text(a_location(:,1)+dx,a_location(:,2)+dy,UAV_ID);
-xlabel('X-axis (m)')
-ylabel('Y-axis (m)')
+% close all
+% figure('position', [650, 10, 400, 350]);hold on;
+% grid on;daspect([1 1 1]);axis square
+% axis([-t_location_limit(1) t_location_limit(1) -t_location_limit(2) t_location_limit(2)]*1.1)
+% axis square
+% grid on;
+% UAV_ID = cellstr(num2str([1:n]'));
+% [UAV_1,UAV_2] = find(triu(MST)>0);
+% n_Edge = length(UAV_1);
+% dx = 4; dy = 1;
+% text(a_location(:,1)+dx,a_location(:,2)+dy,UAV_ID);
+% xlabel('X-axis (m)')
+% ylabel('Y-axis (m)')
 
 
-cc_task = jet(m);
-cc_task = [[0.4 0.4 0.4 ];cc_task];
-final_time = size(output.visual.Satisfied_history,2);
-Marker_size = 4.5;
+% cc_task = jet(m);
+% cc_task = [[0.4 0.4 0.4 ];cc_task];
+% final_time = size(output.visual.Satisfied_history,2);
+% Marker_size = 4.5;
 
-time = 1:10:final_time;
-for k_=1:length(time)
-    k = time(k_);
-    Alloc_ = output.visual.Alloc_history(:,k);
-    Satisfied_ = output.visual.Satisfied_history(:,k);
-    cla;
-
-
-    % Communication network
-    for kk=1:n_Edge
-        i = UAV_1(kk);
-        j = UAV_2(kk);
-
-        plot([a_location(i,1) a_location(j,1)],[a_location(i,2) a_location(j,2)],'-','Color',cc_task(1,:));
-
-    end
+% time = 1:10:final_time;
+% for k_=1:length(time)
+%     k = time(k_);
+%     Alloc_ = output.visual.Alloc_history(:,k);
+%     Satisfied_ = output.visual.Satisfied_history(:,k);
+%     cla;
 
 
-    % Agent position + Allocation
-    for j=1:n
-        plot(a_location(j,1),a_location(j,2),'o','MarkerSize',Marker_size,'MarkerEdgeColor',cc_task(1,:), 'MarkerFaceColor',cc_task(Alloc_(j)+1,:))
-    end
+%     % Communication network
+%     for kk=1:n_Edge
+%         i = UAV_1(kk);
+%         j = UAV_2(kk);
+
+%         plot([a_location(i,1) a_location(j,1)],[a_location(i,2) a_location(j,2)],'-','Color',cc_task(1,:));
+
+%     end
 
 
-    % Task
-    Adjust_factor = 0.5*0.15;
-    for jj=1:m
-        plot(t_location(jj,1),t_location(jj,2),'s','MarkerEdgeColor',cc_task(1,:),'MarkerSize',Marker_size*t_demand(jj)/1000*Adjust_factor, 'MarkerFaceColor',cc_task(jj+1,:))
-
-        txt = ['t',num2str(jj)];
-        text(t_location(jj,1),t_location(jj,2),txt,'HorizontalAlignment','center','FontWeight','bold','Fontsize',20)
-
-    end
+%     % Agent position + Allocation
+%     for j=1:n
+%         plot(a_location(j,1),a_location(j,2),'o','MarkerSize',Marker_size,'MarkerEdgeColor',cc_task(1,:), 'MarkerFaceColor',cc_task(Alloc_(j)+1,:))
+%     end
 
 
-    % Iterations
-    txt = ['# Iterations = ',num2str(output.visual.iteration_history(k))];
-    text(500,500,txt,'HorizontalAlignment','right','Fontsize',13)
+%     % Task
+%     Adjust_factor = 0.5*0.15;
+%     for jj=1:m
+%         plot(t_location(jj,1),t_location(jj,2),'s','MarkerEdgeColor',cc_task(1,:),'MarkerSize',Marker_size*t_demand(jj)/1000*Adjust_factor, 'MarkerFaceColor',cc_task(jj+1,:))
 
-    % For GIF
-    if k_==1
-        f = getframe;
-        [im,map] = rgb2ind(f.cdata,256,'nodither');
-        im(1,1,1,20) = 0;
-    else
-        f = getframe;
-        im(:,:,1,k_) = rgb2ind(f.cdata,map,'nodither');
-    end
+%         txt = ['t',num2str(jj)];
+%         text(t_location(jj,1),t_location(jj,2),txt,'HorizontalAlignment','center','FontWeight','bold','Fontsize',20)
 
-end
-imwrite(im,map,'Result_TA.gif','DelayTime',0,'LoopCount',inf) %g443800
+%     end
+
+
+%     % Iterations
+%     txt = ['# Iterations = ',num2str(output.visual.iteration_history(k))];
+%     text(500,500,txt,'HorizontalAlignment','right','Fontsize',13)
+
+%     % For GIF
+%     if k_==1
+%         f = getframe;
+%         [im,map] = rgb2ind(f.cdata,256,'nodither');
+%         im(1,1,1,20) = 0;
+%     else
+%         f = getframe;
+%         im(:,:,1,k_) = rgb2ind(f.cdata,map,'nodither');
+%     end
+
+% end
+% imwrite(im,map,'Result_TA.gif','DelayTime',0,'LoopCount',inf) %g443800
 
